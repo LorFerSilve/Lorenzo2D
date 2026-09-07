c4690579b2def3eb1543f1570c4b68bc9edaec94
LorFerSilve <silvetnefernandezlorenzo@gmail.com>
2026-09-07 04:01:34 +0200

    Expect Lorenzo2D 0.13.0

--- tests/Lorenzo2DVersionTests.cpp
+++ tests/Lorenzo2DVersionTests.cpp
@@ -12,9 +12,9 @@ namespace
     void testGeneratedVersionMatchesProjectVersion()
     {
         L2D_REQUIRE_EQUAL(l2d::VersionMajor, 0u);
-        L2D_REQUIRE_EQUAL(l2d::VersionMinor, 12u);
+        L2D_REQUIRE_EQUAL(l2d::VersionMinor, 13u);
         L2D_REQUIRE_EQUAL(l2d::VersionPatch, 0u);
-        L2D_REQUIRE_EQUAL(l2d::VersionString, std::string_view("0.12.0"));
+        L2D_REQUIRE_EQUAL(l2d::VersionString, std::string_view("0.13.0"));
     }
 
     void testVersionMacrosMatchConstants()
