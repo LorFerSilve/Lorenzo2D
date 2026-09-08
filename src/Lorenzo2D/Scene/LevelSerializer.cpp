779a5d71f6992ddd7123fe36fe6551d280fad235
LorFerSilve <silvetnefernandezlorenzo@gmail.com>
2026-09-09 01:38:47 +0200

    Make level instantiation transactionally rollback

--- src/Lorenzo2D/Scene/LevelSerializer.cpp
+++ src/Lorenzo2D/Scene/LevelSerializer.cpp
@@ -1001,13 +1001,23 @@ namespace l2d
             throw std::invalid_argument("Cannot instantiate an invalid level document.");
         }
 
+        const std::size_t rollbackIndex = scene.gameObjectCount();
+        const std::size_t rollbackIndex = scene.gameObjectCount();
         std::vector<GameObjectHandle> handles;
         handles.reserve(level.objects.size());
 
-        for (const Prefab& prefab : level.objects)
+        try
         {
-            GameObject& object = instantiatePrefab(scene, prefab);
-            handles.push_back(scene.createHandle(object));
+            for (const Prefab& prefab : level.objects)
+            {
+                GameObject& object = instantiatePrefab(scene, prefab);
+                handles.push_back(scene.createHandle(object));
+            }
+        }
+        catch (...)
+        {
+            scene.rollbackGameObjectsFrom(rollbackIndex);
+            throw;
         }
 
         return handles;
@@ -1048,9 +1058,7 @@ namespace l2d
         }
         catch (...)
         {
-            for (const GameObjectHandle& handle : handles)
-                if (GameObject* object = handle.get()) object->destroy();
-            scene.destroyQueuedGameObjects();
+            scene.rollbackGameObjectsFrom(rollbackIndex);
             throw;
         }
 
