3d1ac9982eeb8361ca663a71582b104761ddf0f0
LorFerSilve <silvetnefernandezlorenzo@gmail.com>
2026-09-09 11:56:03 +0200

    Use one scoped render view guard per pass

--- src/Lorenzo2D/Renderer/RenderPipeline2D.cpp
+++ src/Lorenzo2D/Renderer/RenderPipeline2D.cpp
@@ -14,27 +14,6 @@ namespace l2d
 {
     namespace
     {
-        class RenderTargetViewRestore final
-        {
-          public:
-            explicit RenderTargetViewRestore(sf::RenderTarget& target)
-                : m_target(target), m_view(target.getView())
-            {
-            }
-
-            ~RenderTargetViewRestore()
-            {
-                m_target.setView(m_view);
-            }
-
-            RenderTargetViewRestore(const RenderTargetViewRestore&) = delete;
-            RenderTargetViewRestore& operator=(const RenderTargetViewRestore&) = delete;
-
-          private:
-            sf::RenderTarget& m_target;
-            sf::View m_view;
-        };
-
         bool isValidRenderPass(RenderPass2D pass) noexcept
         {
             switch (pass)
@@ -419,8 +398,6 @@ namespace l2d
                 }
 
                 ScopedRenderView2D viewGuard(*target);
-
-                RenderTargetViewRestore restoreView(*target);
 
                 if (passConfig.clear.enabled)
                 {
